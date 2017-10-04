a6365797eeedd0afa38195b4f4909516a720c411
Jack Kelley <jallynkelley@gmail.com>
2017-10-04 15:32:02 -0500

    initial commit for TF help

---
+++ user-signup/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def index():
+    return render_template('user_signup.html')
+
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    username = request.form['username']
+    return render_template('welcome.html')
+
+
+
+
+
+
+
+app.run()
